3ec029175891bb0f0825a205ccce301682036178
RFuentes22 <00062217@uca.edu.sv>
2019-09-06 21:38:32 -0600

    Muestra todas las preguntas

--- app/src/main/java/com/project/discretaii_project/Activities/Game.kt
+++ app/src/main/java/com/project/discretaii_project/Activities/Game.kt
@@ -5,19 +5,42 @@ import android.os.Bundle
 import com.project.discretaii_project.R
 import kotlinx.android.synthetic.main.activity_game.*
 
+
 class Game : AppCompatActivity() {
 
+    var questions = listOf(R.string.q1,R.string.q2,R.string.q3,R.string.q4,R.string.q5,R.string.q6)
+
+    private var counter = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
         showQuestion()
+        bt_next.setOnClickListener{
+            //nextquestion(2)
+
+            id_question.text = getString(questions[counter])
+            counter ++
+        }
     }
 
     fun showQuestion(){
-        var questions = listOf("Suponiendo que hay X letras distintas. ¿Cuantos conjuntos diferentes de iniciales pueden formarse si cada persona " +
-                "tiene un apellido y exactamente dos nombres?", "Hola","Mundo")
 
-        id_question.text = questions[0]
+        id_question.text = getString(R.string.q1)
+    }
+
+    private fun nextquestion(i: Int): Int{
+        return when (i) {
+            1 -> {
+                counter--
+                if(counter<0) counter = 2
+                questions[counter]
+            }
+            else -> {
+                counter++
+                if(counter>2) counter = 0
+                questions[counter]
+            }
+        }
     }
 }
